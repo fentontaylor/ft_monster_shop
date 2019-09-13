52e3093bab33669099e1d01afb35fcfa67ac2750
Fenton Taylor <fentontaylor@gmail.com>
2019-09-13 12:03:08 -0600

    Refactor factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,11 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) {|x| "Name #{x}"}
-    sequence(:address) {|x| "Address #{x}"}
-    sequence(:city) {|x| "City #{x}"}
-    sequence(:state) {|x| "State #{x}"}
-    zip {rand(10000..99999)}
-    sequence(:email) {|x| "email #{x}"}
+    sequence(:email) {|x| "email_#{x}@email.com"}
     sequence(:password) {|x| "password#{x}"}
     role { 1 }
     association :merchant, factory: :merchant
