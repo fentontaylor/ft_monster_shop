973ed8a0594e2fe11ab1ed3cb0623ce64c96c627
Fenton Taylor <fentontaylor@gmail.com>
2019-09-16 19:59:06 -0600

    Finish sad path test for order update

--- spec/features/orders/edit_spec.rb
+++ spec/features/orders/edit_spec.rb
@@ -1,28 +1,30 @@
 require 'rails_helper'
 
 describe 'User clicks Change Address from pending order show page' do
-  it 'Takes them to a form to edit the shipping details'do
-    user = create(:user)
-    address = create(:address)
-    user.addresses << address
-    item = create(:item)
-    order = user.orders.create(name: user.name, address: address.address, city: address.city, state: address.state, zip: address.zip)
-    item_order = order.item_orders.create(item: item, price: item.price, quantity: 1)
+  before :each do
+    @user = create(:user)
+    @address = create(:address)
+    @user.addresses << @address
+    @item = create(:item)
+    @order = @user.orders.create(name: @user.name, address: @address.address, city: @address.city, state: @address.state, zip: @address.zip)
+    @item_order = @order.item_orders.create(item: @item, price: @item.price, quantity: 1)
 
     visit '/login'
 
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
 
     within '#login-form' do
       click_on 'Log In'
     end
+  end
 
-    visit order_path(order.id)
+  it 'Takes them to a form to edit the shipping details'do
+    visit order_path(@order)
 
     click_link 'Edit Shipping Details'
 
-    expect(current_path).to eq(edit_order_path(order))
+    expect(current_path).to eq(edit_order_path(@order))
     fill_in 'Address', with: '123 Updated Address'
     fill_in 'City', with: 'Edit City'
     select 'Hawaii', from: 'State'
@@ -30,11 +32,26 @@ describe 'User clicks Change Address from pending order show page' do
 
     click_on 'Update Order'
 
-    expect(current_path).to eq(order_path(order.id))
+    expect(current_path).to eq(order_path(@order))
 
     within '.shipping-address' do
       expect(page).to have_content('123 Updated Address')
       expect(page).to have_content('Edit City, HI 99999')
     end
+  end
+
+  it 'Needs to have all fields filled in' do
+    visit edit_order_path(@order)
+
+    fill_in 'Name', with: nil
+    fill_in 'Address', with: nil
+    fill_in 'City', with: nil
+    select '', from: 'State'
+    fill_in 'Zip', with: nil
+
+    click_on 'Update Order'
+    
+    expect(current_path).to eq(edit_order_path(@order))
+    expect(page).to have_content("Name can't be blank, Address can't be blank, City can't be blank, State can't be blank, and Zip can't be blank")
   end
 end
